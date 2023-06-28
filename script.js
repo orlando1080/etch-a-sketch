5ca0c9b90482768211ca915cee73d7a830e2c5a2
Lewis Sewell <lewislsewell@gmail.com>
2023-06-28 17:50:15 +0100

    Add removeGrid, promptGrid functions

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-function createGrid(rows) {
-    const body = document.querySelector('body');
+function createGrid(rows = 16) {
+    const body = document.querySelector('body'),
+          gridContainer = document.createElement('div');
     for (let i = 0; i < rows; i++) {
         const containerRow = document.createElement('div');
         containerRow.classList.add('container-row');
-        body.appendChild(containerRow)
+        gridContainer.appendChild(containerRow);
+      gridContainer.classList.add('grid-container');
         for (let j = 0; j < rows; j++) {
             const containerItem = document.createElement('div');
             containerItem.classList.add('container-item');
             containerRow.appendChild(containerItem);
         }
+        body.appendChild(gridContainer);
     }
-    body.appendChild(containerRow);
+    highlightDiv();
+
 }
 
-createGrid(16);
+function highlightDiv() {
+    const containerItems = document.querySelectorAll('.container-item');
+    containerItems.forEach((item) => {
+        item.addEventListener('mouseover', function() {
+            item.classList.add('hover');
+        });
+    
+    });
+}
+
+function createButton() {
+  const button = document.createElement('button'),
+        div = document.createElement('div'),
+        body = document.querySelector('body');
+  
+  div.classList.add('button-container');
+  button.textContent = 'This is a button';
+  div.appendChild(button);
+  body.appendChild(div);
+  
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', promptGrid);
+}
+
+function removeGrid() {
+  rows = document.querySelectorAll('.container-row');
+  rows.forEach((row) => {
+    row.remove();
+  });
+}
+
+function promptGrid() {
+  const gridSize = prompt('input');
+  removeGrid();
+  createGrid(gridSize);
+}
+
+createButton();
+createGrid();
+
